9a0cf6fa9d1db4cbb9e9baa1cd9e3143b3288e02
ocustodio <cstdpvictor@gmail.com>
2024-05-27 14:11:36 -0300

    variables/conditionals/loops

--- demo/index.php
+++ demo/index.php
@@ -3,12 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: monospace;
+            font-size: 1.125rem;
+            color: #FFFFFF;
+            background-color: #0F0F0F;
+        }
+    </style>
 </head>
 <body>
-    <h1>
-        <?php 
-            echo "Hello, " . "World!";
-        ?>
+    <?php 
+        $name = "Victor";
+        $trained = true;
+
+        $message = "$name. you " . ($trained ? "already" : "did not") . " trained today!";
+
+        $listOfNames = [
+            [
+                "id" => 1,
+                "name" => "Vito",
+                "lastName" => "Custodio",
+            ],
+            [
+                "id" => 2,
+                "name" => "Joao",
+                "lastName" => "Leonardo",
+            ],
+            [
+                "id" => 3,
+                "name" => "Marco",
+                "lastName" => "Polo",
+            ],
+        ];
+    ?>
+
+    <h1> 
+        Hello, <?= $message ?>
     </h1>
+
+    <ul>
+        <?php foreach($listOfNames as $names) : ?>
+            <li>
+                <?= $names["id"] ?> |
+                <?= "{$names["name"]}  {$names["lastName"]}" ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <p>
+        <?= "1st /> " . $listOfNames[0] ?>
+    </p>
 </body>
 </html>
